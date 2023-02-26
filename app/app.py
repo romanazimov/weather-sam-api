79a17cdb0e6917a16fbd1ff41e5b949f517a3987
Roman <36523168+romanazimov@users.noreply.github.com>
2023-02-26 11:22:45 -0500

    Fixed GET and GET List.

--- app/app.py
+++ app/app.py
@@ -1,26 +1,47 @@
 import json
 
-# import requests
-
-
 def lambda_handler(event, context):
-
-    # /weather
-    if event['httpMethod'] == 'GET':
-        return response("GET REQEUST")
     
+    if event['httpMethod'] == 'GET':
+        if event.get('pathParameters') is None:
+            # Return list of items
+            return response("GET LIST REQUEST")
+        else:
+            # Return specific item
+            item_id = event['pathParameters']['id']
+            if item_id.isnumeric():
+                return response(item_id)
+            else:
+                return response("Invalid item ID")
+    
+
     # /weather
     if event['httpMethod'] == 'POST':
         return response("POST REQEUST")
     
+
     # /weather/{id}
     if event['httpMethod'] == 'PUT':
-        return response("PUT REQEUST")
+        item_id = event['pathParameters']['id']
+        if not item_id.isnumeric():
+            return response("Invalid item ID")
+        
+        request_body = json.loads(event['body'])
+        if 'description' in request_body:
+            description = request_body['description']
+            return response(f"Description: {description} and ID: {item_id}")
+        else:
+            return response("Key 'description' not found in request body")
+
 
     # /weather/{id}
     if event['httpMethod'] == 'DELETE':
-        return response("DELETE REQEUST")
-    
+        item_id = event['pathParameters']['id']
+        if item_id.isnumeric():
+            return response(item_id + " DELETE REQUEST")
+        else:                
+            return response("Invalid item ID")
+
 
 def response(message):
     return {
