a3b3603f77b874c7db43740662f148267f2c7bef
Roman <36523168+romanazimov@users.noreply.github.com>
2023-02-25 21:42:52 -0500

    Added the endpoints for the API Gateway on Lambda.

--- app/app.py
+++ app/app.py
@@ -5,10 +5,27 @@ import json
 
 def lambda_handler(event, context):
 
+    # /weather
+    if event['httpMethod'] == 'GET':
+        return response("GET REQEUST")
+    
+    # /weather
+    if event['httpMethod'] == 'POST':
+        return response("POST REQEUST")
+    
+    # /weather/{id}
+    if event['httpMethod'] == 'PUT':
+        return response("PUT REQEUST")
+
+    # /weather/{id}
+    if event['httpMethod'] == 'DELETE':
+        return response("DELETE REQEUST")
+    
+
+def response(message):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
+            "message": message,
+        })
     }
