a0a526f46aa3bb449f12edb59871705f17933989
Roman <36523168+romanazimov@users.noreply.github.com>
2023-03-06 12:26:26 -0500

    Changed to dictionary cursor

--- app/db/connector.py
+++ app/db/connector.py
@@ -2,13 +2,10 @@ import mysql.connector
 
 def connector(query):
     cnx = mysql.connector.connect(host='localhost', database='weather_data', user='root', password='')
-    cursor = cnx.cursor()
-    cursor.execute(query)
+    cursor = cnx.cursor(dictionary=True)
 
-    results = []
-    columns = [column[0] for column in cursor.description]
-    for row in cursor.fetchall():
-        results.append(dict(zip(columns, row)))
+    cursor.execute(query)
+    results = cursor.fetchall()
 
     cursor.close()
     cnx.close()
